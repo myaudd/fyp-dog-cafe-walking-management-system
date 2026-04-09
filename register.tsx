87b051eefd4342e34a0a638b448287096dbcc181
myaudd <166720451+myaudd@users.noreply.github.com>
2026-04-09 17:04:46 +0800

    Add files via upload

--- register.tsx
+++ register.tsx
@@ -1,5 +1,24 @@
+import "./register.css";
+
 const Register = () => {
-    return <h1>Register Page</h1>;
+  return (
+    <div className="register-container">
+      <h1>Register</h1>
+      
+      <div className="form-group">
+        <label>Username</label>
+        <input type="text" placeholder="Enter username" />
+
+        <label>Email</label>
+        <input type="email" id="email" name="email" placeholder="Enter email" />
+
+        <label>Password</label>
+        <input type="password" placeholder="Enter password" />
+      </div>
+
+      <button className="register-button">Create an account</button>
+    </div>
+  );
   };
   
   export default Register;
