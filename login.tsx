e66df5434fe702a09196c8a817ec206ad508036f
myaudd <166720451+myaudd@users.noreply.github.com>
2026-04-09 12:35:13 +0800

    label and input gap fixed

--- login.tsx
+++ login.tsx
@@ -5,12 +5,10 @@ const Login = () => {
     <div className="login-container">
       <h2>Login</h2>
       
-      <div className="username-group">
+      <div className="form-group">
         <label>Username</label>
         <input type="text" placeholder="Enter username" />
-      </div>
-      
-      <div className="password-group">
+
         <label>Password</label>
         <input type="password" placeholder="Enter password" />
       </div>
