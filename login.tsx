bf596c2f242bec6debe0882eaf5ad8f292d8ebb5
myaudd <166720451+myaudd@users.noreply.github.com>
2026-04-13 00:24:47 +0800

    customer and staff login

--- login.tsx
+++ login.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { supabase } from "./supabaseClient";
 import "./login.css";
 
@@ -6,6 +7,7 @@ const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const navigate = useNavigate();
 
   const handleSubmit = async () => {
     setError("");
@@ -16,26 +18,61 @@ const Login = () => {
     }
 
     try {
-      const { data, error } = await supabase
+      const { data: customer} = await supabase
         .from("customer")
         .select("*")
         .eq("customername", username)
-        .single();
+        .maybeSingle();
   
-      if (error || !data) {
-        setError("User not found.");
-        return;
-      }
-  
-      if (data.customerpassword !== password) {
-        setError("Incorrect password.");
+      if(customer) {
+        if (customer.customerpassword !== password) {
+          setError("Incorrect password.");
+          return;
+        }
+
+        const user = {
+          role: "customer",
+          data: customer
+        };
+
+        localStorage.setItem("user", JSON.stringify(user));
+
+        alert("Customer log in successful!");
+
+        navigate("/customer/customerHome");
         return;
       }
 
-    alert("Log in successful!");
-  } catch (err) {
-    setError("Something went wrong.");
-  }
+      const { data: staff} = await supabase
+        .from("staff")
+        .select("*")
+        .eq("staffname", username)
+        .maybeSingle();
+
+      if (staff) {
+        if (staff.staffpassword !== password) {
+          setError("Incorrect password.");
+          return;
+        }
+
+        const user = {
+          role: "staff",
+          data: staff
+        };
+
+        localStorage.setItem("user", JSON.stringify(user));
+
+        alert("Staff log in successful!");
+
+        navigate("/staff/staffHome");
+        return;
+      }
+
+      setError("User not found.");
+
+    } catch (err) {
+      setError("Something went wrong.");
+    }
   };
 
   return (
